0a414fba41d0b0e1c25180839d19a908618bc510
alhamd <basit.005@gmail.com>
2010-11-04 15:30:12 +0530

    u.py is still being worked on

--- u.py
+++ u.py
@@ -1,46 +1,50 @@
 #! /usr/bin/python2
 """This is a module that returns the analytical velocity field once u_i(y) and 
     cos(l_i*z) are know. The variables might be renamed this is just a guide. 
+    m&b refers to text.
 """
+
 from math import cos, sin, hypot, pi
-# Some hacks.
-#H = raw_input("Enter the Hartmann number: ")
-#A = raw_input("Specify the aspect ratio : ")
+TOL = 1e-6
 
-def velocity(ha_, asp_r, z_co):
-    """ Evaluate the velocity at a point for some given Ha and aspect ratio.
-    From the given value of Ha, aspect ratio and z compute the velocity at 
-    that point. H represents the Hartmann number, a parameter for this design.
-    y is defined in mode_ui() so not repeated. 
+def velocity(ha, asp, y_co, z_co):
+    """Sums the fourier modes and obtains the velocity analytical solution
+    From the fourier modes of y and z contributions.
     """
-    
-    d_z = 1/asp_r                             #following muller
-    vel_u = 0.0                                 #init
     i = 1
-    y_co = 0.0
-    u_i = mode_ui(ha_, asp_r, i, y_co)
+    u = 0.0
+    
+    uiy = u_y(ha, asp, i, y_co)
+    uiz = u_z(asp, z_co, i)
 
-    while u_i >= 1.0e-6:
-        u_i = mode_ui(ha_, i, y_co)
-        l_i = 0.5*i*pi/d_z
-        vel_u += u_i
-    
-    return vel_u*cos(l_i*z_co)
-    
-def mode_ui(ha_, asp_r, i, y_co):
-    """Compute an individual mode of the velocity field y function. 
-    The term that is the y component of the fourier series sum 
-    solution.
+    while uiy*uiz >= TOL: 
+        uiy = u_y(ha, asp, y_co, i)
+        uiz = u_z(asp, z_co, i)
+
+        u += uiy*uiz
+        i += 2
+        
+    return u
+
+def u_z(a, z, i):
+    """The z contribution to velocity from formulae in m&b
+    Oneliner form is better. Lambda form is complicated to use?
     """
-    d_z = 1/asp_r
-    l_i = 0.5*i*pi/d_z
-    k_i = 4*sin(0.5*i*pi)/(i*pi)
+    return cos(0.5*a*i*pi*z)
 
-    if (ha_*d_z)/(i*pi) >= 10:
-        return 
-    p_i1 = 0.5*(ha_**2-hypot(ha_, 2*l_i))
-    p_i2 = 0.5*(ha_**2+hypot(ha_, 2*l_i))
+def u_y(h, a, y, i):
+    """The y contribution of velocity
+    Most work goes in here.
+    """
+
+# The solution has a tendency to blowup as we are using hyperbolic 
+# functions. This is to prevent that from occuring
+
+    if h/(a*i*pi) >= 10.0:
+        return 0.0
+
+    return y
     
-    
-    return 
+
+
     
